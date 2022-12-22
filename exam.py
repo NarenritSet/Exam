fc96dc2d757b428f4a1b4675e45f2219cc8d76d7
ZoZayElf <tuey54321@gmail.com>
2022-12-22 20:39:33 +0700

    Add comments, Add input prompt

--- exam.py
+++ exam.py
@@ -2,12 +2,15 @@ from collections import Counter,OrderedDict
 
 
 def countFromInput(x):
+    #Get the string from input, remove exceeded comma, split them into list, then convert items to int with map() function.
     inputlist = list(map(int, x.strip(',').split(',')))
+    #Count items with Counter() then sorted key with sorted()
+    #Now there is a list of items and their amount, finally, create the Dictionary with order by OrderedDict()
     sortedcounter = OrderedDict(sorted(Counter(inputlist).items()))
     for k,v in sortedcounter.items():
         print(f"{k}:{v}")
 
-x = input()
+x = input('Input: ')
 try:
     countFromInput(x)
 except:
